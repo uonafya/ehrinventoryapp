f5a6a000d4a8b4c40b39de1cbac2b02d01056962
ningosi <nicotwendelee@gmail.com>
2021-06-10 20:43:37 +0300

    Complete the schedule information

--- api/src/main/java/org/openmrs/module/ehrinventoryapp/task/CopyDrugsFromOpenmrsDrugToInventoryDrug.java
+++ api/src/main/java/org/openmrs/module/ehrinventoryapp/task/CopyDrugsFromOpenmrsDrugToInventoryDrug.java
@@ -5,9 +5,15 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.ehrinventory.InventoryService;
 import org.openmrs.module.hospitalcore.InventoryCommonService;
 import org.openmrs.module.hospitalcore.model.InventoryDrug;
+import org.openmrs.module.hospitalcore.model.InventoryDrugFormulation;
+import org.openmrs.module.hospitalcore.model.InventoryDrugUnit;
 import org.openmrs.scheduler.tasks.AbstractTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CopyDrugsFromOpenmrsDrugToInventoryDrug extends AbstractTask {
 
@@ -30,11 +36,28 @@ public class CopyDrugsFromOpenmrsDrugToInventoryDrug extends AbstractTask {
                     if(inventoryCommonService.getDrugByName(drug.getName()) == null){
                         System.out.println("The drug is NOT created, we need it created in the inventory drug>>"+drug.getName());
                         //create a new inventory drug object
-                        InventoryDrug inventoryDrug = new InventoryDrug();
+                        //check if there is drug units in the database
 
+                        if(inventoryService.getDrugUnitById(1) != null && inventoryService.getDrugCategoryById(1) != null && inventoryService.getDrugFormulationById(1) != null) {
+                            System.out.println("At least the required parameters to set have been provided");
+                            Set<InventoryDrugFormulation> formulations = new HashSet<InventoryDrugFormulation>();
+                            formulations.add(inventoryService.getDrugFormulationById(1));
 
-                        //save the inventry drugs as formulated
-                        inventoryService.saveDrug(inventoryDrug);
+                            InventoryDrug inventoryDrug = new InventoryDrug();
+                            inventoryDrug.setName("All drugs");
+                            inventoryDrug.setUnit(inventoryService.getDrugUnitById(1));
+                            inventoryDrug.setCategory(inventoryService.getDrugCategoryById(1));
+                            inventoryDrug.setFormulations(formulations);
+                            inventoryDrug.setCreatedOn(new Date());
+                            inventoryDrug.setCreatedBy(Context.getAuthenticatedUser().getGivenName());
+                            inventoryDrug.setDrugCore(drug);
+                            inventoryDrug.setAttribute(1);
+                            inventoryDrug.setReorderQty(100);
+                            inventoryDrug.setConsumption(10);
+
+                            //save the inventry drugs as formulated
+                            inventoryService.saveDrug(inventoryDrug);
+                        }
 
                     }
 
